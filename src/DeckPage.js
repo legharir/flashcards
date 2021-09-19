c8f081d4bad12f1c88c378b5ccb3e16247f05cd7
Rafay Leghari <rafayaleghari@gmail.com>
2021-09-19 16:59:30 -0400

    Add ability to delete decks

--- src/DeckPage.js
+++ src/DeckPage.js
@@ -15,13 +15,56 @@ function DeckPage({ decks, setDecks }) {
     setNewDeckCreated(true);
   };
 
+  const onDeleteDeck = (deckName) => {
+    if (
+      !window.confirm(`Are you sure you wish to delete the ${deckName} deck?`)
+    ) {
+      return;
+    }
+
+    const req = indexedDB.open("flashcards", 1);
+
+    req.onerror = (e) => {
+      console.error(
+        "Could not open connection to database, error code: ",
+        e.target.errorCode
+      );
+    };
+
+    req.onsuccess = (e) => {
+      const db = e.target.result;
+      db.onerror = (e) => console.error(e.target.errorCode);
+
+      const transaction = db.transaction("decks", "readwrite");
+      const objectStore = transaction.objectStore("decks");
+      const request = objectStore.delete(deckName);
+      request.onsuccess = (e) => {
+        setDecks(decks.filter((deck) => deck !== deckName));
+      };
+    };
+
+    req.onupgradeneeded = (e) => {
+      console.log("Performing DB migration...");
+      const db = e.target.result;
+      db.createObjectStore("decks", { keyPath: "name" });
+    };
+  };
+
   return (
     <Container>
       {newDeckCreated && <Redirect to={`/deck/${newDeckName}`} />}
 
       {decks.map((deck) => (
-        <div key={deck}>
-          <Link to={`/deck/${deck}`}>{deck}</Link>
+        <div style={{ margin: "0.5em" }} key={deck}>
+          <Link to={`/deck/${deck}`} style={{ marginRight: "1em" }}>
+            {deck}
+          </Link>
+          <button
+            className="btn btn-danger btn-sm"
+            onClick={() => onDeleteDeck(deck)}
+          >
+            Delete
+          </button>
         </div>
       ))}
 
